5e31b6cab66ddc20bd65ba38630a5a8648197a30
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-10 09:12:12 -0700

    got the card build properly without the axios stuff.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,30 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+function Card(article) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    author.appendChild(authorName);
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imageContainer.classList.add('img-container');
+
+    headline.textContent = "SMARCH 28, 2019";
+    image.src = "#";
+    authorName.textContent = "By ";
+    // console.log(header);
+    return card;
+}
+console.log(Card({}))
+// headerContainer.appendChild(Header());
